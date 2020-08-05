706e13c72a42b0656aa748340f62e3aed52d4093
Jeremy Block <j.block@ufl.edu>
2020-08-05 11:19:44 -0400

    Major: dynamic component load on app entry

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,56 +1,38 @@
 <template>
   <div>
-    <v-alert
-      text
-      outlined
-      color="deep-orange"
-      icon="mdi-alert"
-    >
-    This study is still being developed. To check on any particular page, click a link below.
-    </v-alert>
-    <h2><router-link :to="{name:'Consent',query: {id: this.userID, src: this.src} }">Consent</router-link> </h2>
-    <h2><router-link :to="{name:'Tutorial'}" replace>Tutorial</router-link> </h2>
-    <h2><router-link :to="{name:'Analysis'}" replace>Begin Analysis</router-link></h2>
-    <h2><router-link :to="{name:'Survey'}" replace>Survey</router-link> </h2>
-    <h2><router-link :to="{name:'Debrief', query: {id: this.userID, src: this.src } }" replace>Debrief</router-link> </h2>
-    <h2><router-link :to="{name:'Cancel'}" replace>Cancel</router-link> </h2>
-    <v-divider></v-divider>
-    <v-alert class="my-4" text color="green"> 
-      Random User ID for this session: {{userID}} |
-      Condition: {{conditions[condID].text}} | 
-      Source: {{src}}
-    </v-alert>
-    <small> 
-      <v-btn x-small @click="setUp()"> Reset User</v-btn> or reload the page
-    </small>
-    <!-- <v-container class="px-0" fluid>
-    <v-radio-group v-model="radioGroup" >
-      <v-radio
-      @click="setCondition(n.id)"
-        v-for="n in conditions"
-        :key="n.id"
-        :label="`Condition: ${n.text}`"
-        :value="n.id"
-      ></v-radio>
-    </v-radio-group>
-  </v-container> -->
+    <Debrief v-if="entrypoint === 'fin'"/>
+    <div v-else-if="entrypoint === 'dev'">
+      <v-alert
+        text
+        outlined
+        color="deep-orange"
+        icon="mdi-alert"
+      >
+      This study is still being developed. To check on any particular page, click a link below.
+      </v-alert>
+      <h2><router-link :to="{name:'Consent',query: {id: this.userID, src: this.src} }">Consent</router-link> </h2>
+      <h2><router-link :to="{name:'Tutorial'}" replace>Tutorial</router-link> </h2>
+      <h2><router-link :to="{name:'Analysis'}" replace>Begin Analysis</router-link></h2>
+      <h2><router-link :to="{name:'Survey'}" replace>Survey</router-link> </h2>
+      <h2><router-link :to="{name:'Debrief', query: {id: this.userID, src: this.src } }" replace>Debrief</router-link> </h2>
+      <h2><router-link :to="{name:'Cancel'}" replace>Cancel</router-link> </h2>
+      <v-divider></v-divider>
+      <v-alert class="my-4" text color="green"> 
+        Random User ID for this session: {{userID}} |
+        Source: {{src}}
+      </v-alert>
+      <small> 
+        <v-btn x-small @click="setUp()"> Reset User</v-btn> or reload the page
+      </small>
+    </div>
+    <Consent v-else />
   </div>
 </template>
 
 <script>
-  let conditions = 
-  [{
-    id:0,
-    text: "Control"
-  },{
-    id:1,
-    text: "Counterfactual"
-  // },{
-  //   id:2,
-  //   text: "Contrastive"
-  }]
+import Consent from "../views/Consent";
+import Debrief from "../views/Debrief";
   let userID=Math.floor(Math.random()*9000)+1000;
-  let condID = userID%conditions.length;
   let src = (Math.random() <0.5) ? 'SONA' : 'ONLINE'
 
 
@@ -58,28 +40,22 @@ export default {
   name: 'Home',
     data () {
       return {
-        conditions,
-        condID,
         userID,
         src,
-        radioGroup: 1,
+        entrypoint: (undefined === this.$route.query.ep) ? "ELSEWHERE": this.$route.query.ep,
       }
     },
+    components: {
+      Consent, Debrief
+    },
     created: ()=> {
-      console.log(userID, condID, src)
-      // sessionStorage.setItem('userID',userID);
-      // sessionStorage.setItem('cond', condID);
-      // sessionStorage.setItem('src',src);
+      console.log(userID, src)
     },
     methods :{
       setUp(){
         console.log("setup");
         this.userID=Math.floor(Math.random()*9000)+1000;
-        this.condID = this.userID%conditions.length;
         this.src = (Math.random() <0.5) ? 'SONA' : 'ONLINE'
-        // sessionStorage.setItem('userID',this.userID);
-        sessionStorage.setItem('cond', this.condID);
-        // sessionStorage.setItem('src',this.src);
         }
     }
 }
