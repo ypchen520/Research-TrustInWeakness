b749364c1c10a53deb1951bc0fcfb7686cb915e7
Jeremy Block <j.block@ufl.edu>
2020-08-09 23:03:37 -0400

    Updating application Title and config file to try preventing zooming in with the browser. Not sure that the meta tag fixes it...

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,15 @@
 <template>
   <v-app>
-<link href="https://cdn.jsdelivr.net/npm/@mdi/font@5.x/css/materialdesignicons.min.css" rel="stylesheet">
+    <head>
+    <title><%= htmlWebpackPlugin.options.title %></title>
+    <meta charset="utf-8"/>
+    <link href="https://cdn.jsdelivr.net/npm/@mdi/font@5.x/css/materialdesignicons.min.css" rel="stylesheet">
+    </head>
 
     <v-app-bar flat app style="z-index: 6">
       <v-toolbar-title>
         Trash Selector
+        <!-- TODO: Remove Router-Link prior to final production build. -->
             <router-link :to="{name:'Home', query:{ep:'dev'}}" replace><v-btn x-small color="secondary" dark>home</v-btn></router-link>
       </v-toolbar-title>
     </v-app-bar>
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,14 @@
 module.exports = {
     //Ternary Operator for root path to files when bundling application for production or development.
-    publicPath: process.env.NODE_ENV === 'production' ? '/TrashSelector/' : '/'  
+    publicPath: process.env.NODE_ENV === 'production' ? '/TrashSelector/' : '/',
+    chainWebpack: config => {
+        config
+        .plugin('html')
+        .tap(args => {
+            args[0].title = 'Trash Selector';
+            args[0].meta = {viewport: 'width=device-width,initial-scale=1,maximum-scale=1,user-scalable=no'};
+
+         return args;
+    })
+    }
 }
